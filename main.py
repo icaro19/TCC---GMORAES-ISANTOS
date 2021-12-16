ccc05b4730dec8ed00a6261ac5d36904b5536361
G-Moraes <gmoraes@ufrrj.br>
2021-12-15 21:23:34 -0300

    predição com múltiplas execuções e cálculo de média

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from randomForest import RF_TCC_DifferentTrainSizesPredict
+from randomForest import RF_TCC_DifferentTrainSizesPredict, RF_TCC_PredictMeans
 
 from fix_set import FP_TCC_CreateFixFingerprints
 from mix_set import FP_TCC_CreateMixFingerprints
@@ -7,10 +7,14 @@ from mix_set import FP_TCC_CreateMixFingerprints
 
 x, y = FP_TCC_CreateFixFingerprints()
 
+RF_TCC_PredictMeans(x, y, 0)
+
 x_mix, y_mix = FP_TCC_CreateMixFingerprints()
+
+RF_TCC_PredictMeans(x, y, 1)
 
 x.extend(x_mix)
 
 y.extend(y_mix)
 
-RF_TCC_DifferentTrainSizesPredict(x, y)
+RF_TCC_PredictMeans(x, y, 2)
--- randomForest.py
+++ randomForest.py
@@ -1,12 +1,16 @@
 import numpy as np
 
 import matplotlib.pyplot as plt
+import numpy as np
 
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import confusion_matrix
+
+import pprint 
+
 
 def RF_TCC_Predict(model, x_treino, y_treino, x_teste, y_teste):
    
@@ -137,7 +141,166 @@ def RF_TCC_DifferentTrainSizesPredict(x, y):
         accuracy.append(medidas[i]['accuracy'])
         f.write('Model scores with ' + str(indexSizeTest[i]) + '%' + ' of whole train dataset:\n' + str(stringMedidas[i]) + '\n')
 
-    RF_TCC_CreateGraph([1, 2, 5, 10, 25, 50, 75, 100], accuracy, 'tamanho do conjunto de teste (%)', 'Acurácia (porção decimal)')
+    RF_TCC_CreateGraph([1, 2, 5, 10, 25, 50, 75, 100], accuracy, 'tamanho do conjunto de treino (%)', 'Acurácia (porção decimal)')
+
+    f.close()
+
+def RF_TCC_GenerateReports(x_treino, y_treino, x_teste, y_teste, size, model):
+
+    if size != 1.0:
+
+        x_treino, x_testeDescartável, y_treino, y_testeDescartável = train_test_split(x_treino, y_treino, train_size = size, shuffle = False, stratify = None)
+        
+    else:
+        pass
+
+    cm = np.array([0.0,0.0,0.0,0.0,0.0,0.0,0.0])
+
+    cm = np.array([cm,cm,cm,cm,cm,cm,cm])
+
+    labels = ['banheiro', 'corredor', 'cozinha', 'quarto 1', 'quarto 2', 'quarto 3', 'sala']
+
+    metricasMediasDict = {
+
+                            'banheiro': {'precision': 0, 'recall': 0, 'f1-score': 0, 'support': 0},
+                            'corredor': {'precision': 0, 'recall': 0, 'f1-score': 0, 'support': 0},
+                            'cozinha': {'precision': 0, 'recall': 0, 'f1-score': 0, 'support': 0},
+                            'quarto 1': {'precision': 0, 'recall': 0, 'f1-score': 0, 'support': 0},
+                            'quarto 2': {'precision': 0, 'recall': 0, 'f1-score': 0, 'support': 0},
+                            'quarto 3': {'precision': 0, 'recall': 0, 'f1-score': 0, 'support': 0},
+                            'sala': {'precision': 0, 'recall': 0, 'f1-score': 0, 'support': 0},
+                            'accuracy': 0
+                         }
+
+    for _ in range(10):
+
+        model.fit(x_treino, y_treino)
+        resultado = model.predict(x_teste)
+        report = classification_report(y_teste, resultado, output_dict = True)
+
+        for label, dictValores in report.items():
+
+            if isinstance(dictValores, dict):
+
+                for metricas in dictValores:
+
+                    if label in labels:
+
+                        metricasMediasDict[label][metricas] += (dictValores[metricas]/10)
+                
+            
+        metricasMediasDict['accuracy'] += ( report['accuracy'] / 10)
+        
+        cm += confusion_matrix(y_teste, resultado, labels = labels)
+
+        cm = np.around(cm, 2)
+
+    cm /= 10
+
+    cm = cm.tolist()
+
+    return metricasMediasDict, cm
+
+def RF_TCC_PredictMeans(x, y, tipo):
+
+    medidas = []
+    stringMatrizes = []
+    accuracy = []
+
+    tipoGrafico = {
+                    0: ['metrics_centralizado.txt', 'grafico_acuracia_centralizado.png', 'Gráfico de acurácia de D_centralizado'],
+                    1: ['metrics_distribuido.txt', 'grafico_acuracia_distribuido.png', 'Gráfico de acurácia de D_distribuido'],
+                    2: ['metrics_unidos.txt', 'grafico_acuracia_unidos.png', 'Gráfico de acurácia de D_centralizado + D_distribuido']
+                  }
+
+    f = open(tipoGrafico[tipo][0], "w+")
+
+    #criando o modelo com os hiper-parâmetros obtidos
+    model = RandomForestClassifier(n_estimators = 5,
+                                   max_depth = 4,
+                                   min_samples_leaf = 2,
+                                   min_samples_split = 2,
+                                   max_leaf_nodes = 10,
+                                   max_features = 'auto'
+                                   )
+
+    #gerando o treino e teste básicos
+    x_treino, x_teste, y_treino, y_teste = train_test_split(x, y, test_size = 0.3)
+
+    #gerando um conjunto de 1% do treino
+    resultado1percent, cm1percent = RF_TCC_GenerateReports(x_treino, y_treino, x_teste, y_teste, 0.01, model)
+    medidas.append(resultado1percent)
+    stringMatrizes.append(cm1percent)
+
+    #gerando um conjunto de 2% do treino
+    resultado2percent, cm2percent = RF_TCC_GenerateReports(x_treino, y_treino, x_teste, y_teste, 0.02, model)
+    medidas.append(resultado2percent)
+    stringMatrizes.append(cm2percent)
+
+    #gerando um conjunto de 5% do treino
+    resultado5percent, cm5percent = RF_TCC_GenerateReports(x_treino, y_treino, x_teste, y_teste, 0.05, model)
+    medidas.append(resultado5percent)
+    stringMatrizes.append(cm5percent)
+
+    #gerando um conjunto de 10% do treino
+    resultado10percent, cm10percent = RF_TCC_GenerateReports(x_treino, y_treino, x_teste, y_teste, 0.1, model)
+    medidas.append(resultado10percent)
+    stringMatrizes.append(cm10percent)
+
+    #gerando um conjunto de 25% do treino           
+    resultado25percent, cm25percent = RF_TCC_GenerateReports(x_treino, y_treino, x_teste, y_teste, 0.25, model)
+    medidas.append(resultado25percent)
+    stringMatrizes.append(cm25percent)
+
+    #gerando um conjunto de 50% do treino
+    resultado50percent, cm50percent = RF_TCC_GenerateReports(x_treino, y_treino, x_teste, y_teste, 0.5, model)
+    medidas.append(resultado50percent)
+    stringMatrizes.append(cm50percent)
+
+    #gerando um conjunto de 75% do treino
+    resultado75percent, cm75percent = RF_TCC_GenerateReports(x_treino, y_treino, x_teste, y_teste, 0.75, model)
+    medidas.append(resultado75percent)
+    stringMatrizes.append(cm75percent)
+
+    #rodando o modelo com 100% do treino
+    resultado100percent, cm100percent = RF_TCC_GenerateReports(x_treino, y_treino, x_teste, y_teste, 1.0, model)
+    medidas.append(resultado100percent)
+    stringMatrizes.append(cm100percent)
+
+    indexSizeTest = {0: 1,
+                     1: 2,
+                     2: 5,
+                     3: 10,
+                     4: 25,
+                     5: 50,
+                     6: 75,
+                     7: 100
+                    }
+
+    for i in range(len(stringMatrizes)):
+
+        f.write('Confusion matrix with ' + str(indexSizeTest[i]) + '%' + ' of D_Treino]:\n')
+        
+        for j in range(len(cm1percent)):
+
+            f.write(str(stringMatrizes[i][j]) + '\n')
+
+        f.write('\n')
+
+    for i in range(len(medidas)):
+        accuracy.append(medidas[i]['accuracy'])
+        f.write('Model scores with ' + str(indexSizeTest[i]) + '%' + ' of whole train dataset:\n')
+        
+        for key in medidas[i].keys():
+
+            f.write(str(key) + ': ' + str(medidas[i][key]) + '\n')
+
+    
+
+    RF_TCC_CreateGraph([1, 2, 5, 10, 25, 50, 75, 100], accuracy, 'tamanho do conjunto de treino (%)', 'Acurácia (porção decimal)', tipoGrafico[tipo])
+
+    f.write(str(cm1percent))
+    f.write(str(resultado1percent))
 
     f.close()
 
@@ -185,18 +348,20 @@ def RF_TCC_FindBestParams(x, y):
 
     return(rf_grid.best_params_, stringAcc)
 
-def RF_TCC_CreateGraph(x, y, x_name, y_name):
+def RF_TCC_CreateGraph(x, y, x_name, y_name, tipo):
 
     plt.xlabel(x_name)
     plt.ylabel(y_name)
 
     default_x_ticks = range(len(x))
 
-    plt.plot(default_x_ticks, y)
+    plt.plot(default_x_ticks, y, "o")
 
-    plt.ylim([0.5, 1.0])
+    plt.ylim([0.8, 1.0])
     plt.xticks(default_x_ticks, x)
 
-    plt.savefig('acuraciaTamanhoConjuntoTreinos.png')
+    plt.title(tipo[2])
+
+    plt.savefig(tipo[1])
 
     plt.close()
